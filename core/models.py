812927c01ec8ca7e19ce1e84f5dcf55b4a4cc349
WiliamDev <wiliam.omura3@hotmail.com>
2025-12-02 21:16:39 -0300

    Djando-admin

---
+++ core/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    rua = models.CharField(max_length=50)
+    bairro = models.CharField(max_length=100)
+    telefone = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
